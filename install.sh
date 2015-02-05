9edcca79cfe50f284f18550a7a89f68dfffc4961
Rahil Momin <rahil.rules.you@gmail.com>
2015-02-05 14:53:08 +0530

    Add install file (not tested yet)

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Dependencies: vim, zsh"
+#TODO: Use readline/dirname to get directory of script
+
+# Setup vim
+ln -s $PWD/vim/vim $HOME/.vim
+ln -s $HOME/.vim/vimrc $HOME/.vimrc
+ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
+
+# Setup zsh
+git clone https://github.com/zsh-users/antigen.git $HOME/.antigen #FixMe: Standardaize
+ln -s $PWD/zsh/zshrc $HOME/.zshrc
+ln -s $PWD/zsh/zshrc.alias $HOME/.zshrc.alias #TODO: Remove this;
