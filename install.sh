201bdd20346beccd1bcb5b252090dfb7bfc553e9
Rahil Momin <rahil.rules.you@gmail.com>
2015-02-05 15:08:41 +0530

    typo fix

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 
 # Setup zsh
-git clone https://github.com/zsh-users/antigen.git $HOME/.antigen #FixMe: Standardaize
+git clone https://github.com/zsh-users/antigen.git $HOME/.antigen #FixMe: Standardize
 ln -s $PWD/zsh/zshrc $HOME/.zshrc
 ln -s $PWD/zsh/zshrc.alias $HOME/.zshrc.alias #TODO: Remove this;
