656b52bae0008442120f8d8d41a01a0ae4edbd21
Rahil Momin <rahil.rules.you@gmail.com>
2015-11-16 13:48:25 +0530

    vim: setup neovim config linked to vim's

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ ln -s $PWD/vim $HOME/.vim
 ln -s $HOME/.vim/init.vim $HOME/.vimrc
 ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
 
+# Setup neovim
+mkdir -p $HOME/.config
+ln -s $PWD/vim $HOME/.config/nvim
+
 # Setup zsh
 git clone https://github.com/zsh-users/antigen.git $HOME/.antigen #FixMe: Standardize
 ln -s $PWD/zsh/zshrc $HOME/.zshrc
