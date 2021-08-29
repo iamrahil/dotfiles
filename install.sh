1f24a48d4ae0a0f2f304934ff382bf9e139d3692
Rahil Momin <rahil@hyperverge.co>
2021-08-29 09:43:32 +0530

    Fix lint in install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,22 @@ echo "Dependencies: vim, zsh"
 #TODO: Use readline/dirname to get directory of script
 
 # Setup vim
-ln -s $PWD/vim $HOME/.vim
-ln -s $HOME/.vim/init.vim $HOME/.vimrc
-ln -s $HOME/.vim/gvimrc $HOME/.gvimrc
+ln -s "$PWD"/vim "$HOME"/.vim
+ln -s "$HOME"/.vim/init.vim "$HOME"/.vimrc
+ln -s "$HOME"/.vim/gvimrc "$HOME"/.gvimrc
 
 # Setup neovim
-mkdir -p $HOME/.config
-ln -s $PWD/vim $HOME/.config/nvim
+mkdir -p "$HOME"/.config
+ln -s "$PWD"/vim "$HOME"/.config/nvim
 
 # Setup tmux
-ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s "$PWD"/tmux/tmux.conf "$HOME"/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
 # Setup zsh
-git clone https://github.com/zsh-users/antigen.git $HOME/.antigen #FixMe: Standardize
-ln -s $PWD/zsh/zshrc $HOME/.zshrc
-ln -s $PWD/zsh/zshrc.alias $HOME/.zshrc.alias #TODO: Remove this;
+git clone https://github.com/zsh-users/antigen.git "$HOME"/.antigen #FixMe: Standardize
+ln -s "$PWD"/zsh/zshrc "$HOME"/.zshrc
+ln -s "$PWD"/zsh/zshrc.alias "$HOME"/.zshrc.alias #TODO: Remove this;
 
 # Init vim
 vim +PlugInstall +qall
@@ -28,5 +28,5 @@ vim +PlugInstall +qall
 chsh -s /bin/zsh
 
 # Init zsh and antigen plugins
-touch $HOME/.profile  # Make sure this file exists to avoid warnings
+touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
 zsh -l
