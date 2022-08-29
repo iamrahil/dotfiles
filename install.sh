ecdfb76f0117a50f83731a400a1b3fd9377385a2
Rahil Momin <rahil.rules.you@gmail.com>
2022-08-29 16:09:41 +0530

    Move zsh install script into zsh/

--- install.sh
+++ install.sh
@@ -6,13 +6,7 @@ bash vim/install.sh
 ln -s "$PWD"/tmux/tmux.conf "$HOME"/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
-# Setup zsh
-git clone https://github.com/zplug/zplug "$HOME"/.zplug
-ln -s "$PWD"/zsh/zshrc "$HOME"/.zshrc
-ln -s "$PWD"/zsh/zshrc.alias "$HOME"/.zshrc.alias #TODO: Remove this;
-
-# Set zsh as default shell
-#chsh -s /bin/zsh
+bash zsh/install.sh
 
 # Init zsh and antigen plugins
 touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
---
+++ zsh/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=$(dirname "$(readlink -f "$0")")
+
+git clone https://github.com/zplug/zplug "$HOME"/.zplug
+ln -s "$DIR"/zshrc "$HOME"/.zshrc
+ln -s "$DIR"/zshrc.alias "$HOME"/.zshrc.alias #TODO: Remove this;
+
+touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
