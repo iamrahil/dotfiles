e96607214fe521e3117f9903a8d39745b8278d2f
Rahil Momin <rahil.rules.you@gmail.com>
2022-08-29 16:35:48 +0530

    Remove duplicate code

--- install.sh
+++ install.sh
@@ -8,6 +8,4 @@ git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
 
 bash zsh/install.sh
 
-# Init zsh and antigen plugins
-touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
 zsh -l
