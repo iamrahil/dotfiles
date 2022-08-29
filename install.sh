927505df69fafcefd930128ad445a53cea799f14
Rahil Momin <rahil.momin@clarisights.com>
2022-08-29 15:14:34 +0530

    Disable setting zsh as default shell
    Preparation to revamp zsh configs

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ ln -s "$PWD"/zsh/zshrc "$HOME"/.zshrc
 ln -s "$PWD"/zsh/zshrc.alias "$HOME"/.zshrc.alias #TODO: Remove this;
 
 # Set zsh as default shell
-chsh -s /bin/zsh
+#chsh -s /bin/zsh
 
 # Init zsh and antigen plugins
 touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
