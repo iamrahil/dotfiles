272921b451543d408f32f796ea6413a05a3aaf39
Rahil Momin <rahil.rules.you@gmail.com>
2022-09-27 17:29:29 +0530

    vim: Remove vim installation configs

--- vim/install.sh
+++ vim/install.sh
@@ -3,14 +3,10 @@
 echo "Dependencies: vim, zsh"
 
 DIR=$(dirname "$(readlink -f "$0")")
-# Setup vim
-ln -s "$DIR" "$HOME"/.vim
-ln -s "$HOME"/.vim/init.vim "$HOME"/.vimrc
-ln -s "$HOME"/.vim/gvimrc "$HOME"/.gvimrc
 
 # Setup neovim
 mkdir -p "$HOME"/.config
 ln -s "$DIR" "$HOME"/.config/nvim
 
 # Init vim
-vim +PlugInstall +qall
+nvim +PlugInstall +qall
