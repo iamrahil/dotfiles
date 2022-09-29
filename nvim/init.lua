a38c34fc2bc3f65748179ff8f8d63536f212b95e
Rahil Momin <rahil.rules.you@gmail.com>
2022-09-29 10:02:32 +0000

    nvim: Add vim-metals for scala projects

--- nvim/init.lua
+++ nvim/init.lua
@@ -91,6 +91,17 @@ cmd([[
 ]])
 require('configs')
 
+
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "scala", "sbt", "java" },
+  callback = function()
+    require("metals").initialize_or_attach({})
+  end,
+  group = nvim_metals_group,
+})
+vim.opt_global.shortmess:remove("F")
+
 require "telescope".setup {
   pickers = {
     colorscheme = {
