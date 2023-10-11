3cf3f50e7ec62da1eeb30e84d32a6838a2d9c8dc
Rahil Momin <rahil.rules.you@gmail.com>
2023-10-11 06:52:54 +0000

    zsh: Add support for before and after scripts in zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,4 +6,7 @@ git clone https://github.com/zplug/zplug "$HOME"/.zplug
 ln -s "$DIR"/zshrc "$HOME"/.zshrc
 ln -s "$DIR"/zshrc.alias "$HOME"/.zshrc.alias #TODO: Remove this;
 
+touch ~/.zshrc.before
+touch ~/.zshrc.after
+
 touch "$HOME"/.profile  # Make sure this file exists to avoid warnings
