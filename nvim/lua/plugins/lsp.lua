df8031e3fbe7466ec25f97fe790b2beaba9dfe09
Rahil Momin <rahil.rules.you@gmail.com>
2025-04-10 10:26:43 +0000

    nvim: Update cinnamon config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -38,5 +38,5 @@ return {
     "j-hui/fidget.nvim",
     tag = "legacy",
     config = true
-  }
+  },
 }
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -29,7 +29,9 @@ return {
     'declancm/cinnamon.nvim',
     config = function()
       require('cinnamon').setup({
-        default_delay = 2
+        options = {
+          delay = 2
+        }
       })
     end
   },
